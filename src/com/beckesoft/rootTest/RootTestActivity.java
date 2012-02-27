ac0ef293c86b21544fa805a4c5b80204fd46ee03
Benedict Becker <benedict.becker93@googlemail.com>
2012-02-27 18:18:34 +0100

    Some nasty code added

---
+++ src/com/beckesoft/rootTest/RootTestActivity.java
@@ -0,0 +1,41 @@
+package com.beckesoft.rootTest;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class RootTestActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //TextView tv = (TextView) findViewById(R.id.test);
+        
+        java.lang.Process p;
+        try {
+           p = Runtime.getRuntime().exec("su");
+           DataOutputStream os = new DataOutputStream(p.getOutputStream());
+           os.writeBytes("echo 'Hi Mom!!!!!!!!' >/system/ciccio.txtn");
+           os.writeBytes("exitn");
+           os.flush();
+           try {
+              p.waitFor();
+                   if (p.exitValue() != 255) {
+                      //tv.setText("OK!");
+                   }
+                   else {
+                      //tv.setText("Error");   
+                   }
+           } catch (InterruptedException e) {
+              // TODO Auto-generated catch block
+              e.printStackTrace();
+           }
+        } catch (IOException e) {
+           // TODO Auto-generated catch block
+           e.printStackTrace();
+        }
+    }
+}
